191538faa98f081072722b4458c6ecff2676dc55
Chau Tran <chau.t.tran.us@gmail.com>
2022-02-19 17:50:56 -0500

    Implemented stock grabber

---
+++ api/get_stats.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import pandas as pd 
+import pandas_ta as ta
+import json
+
+
+def get_stats(stock_data):
+    stock_data.ta.strategy(ta.AllStrategy)
+    return stock_data.to_json()
+
+
+# Testing
+if __name__ == "__main__":
+    data = yf.Ticker("MSFT")
+    data_points = data.history(period="2y")[['Open', 'High', 'Low', 'Close', 'Volume']]
+    data_json = get_stats(data_points)
