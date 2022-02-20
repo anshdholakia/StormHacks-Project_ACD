f2f480db4831747e6d038598b7880219d9e52ab1
Chau Tran <chau.t.tran.us@gmail.com>
2022-02-20 02:13:49 -0500

    replaced all strategies with common strategies

--- api/get_stats.py
+++ api/get_stats.py
@@ -5,7 +5,7 @@ import json
 
 
 def get_stats(stock_data):
-    stock_data.ta.strategy(ta.AllStrategy)
+    stock_data.ta.strategy(ta.CommonStrategy)
     return stock_data
 
 
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     print(data.info)
     data_points = data.history(period="2y")[['Open', 'High', 'Low', 'Close', 'Volume']]
     data_json = get_stats(data_points)
-
+    with open('test.json', 'w') as f:
+        f.write(data_json.to_json(date_format='epoch'))
